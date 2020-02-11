538e2455dc66b2645189f270b18f0e69cd57dacc
Mario Amora <mario.jma@gmail.com>
2020-02-10 22:25:14 -0300

    refactoring for TodoList to use hooks

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,51 +1,38 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as TodoActions from "./store/actions/todos";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, toggleTodo, removeTodo } from './store/actions/todos'
 
-class TodoList extends Component {
+export const TodoList = () => {
 
-  handleSubmit = e => {
+  const [ input, setInput ] = useState("")
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch()
+
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.addTodo(this.input.value);
-    this.input.value = "";
+    dispatch( addTodo(input) );
+    setInput("");
   }
 
-  render(){
-    const { todos, toggleTodo, removeTodo } = this.props;
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={e => setInput(e.target.value)}/>
+        <button type="submit">New</button>
+      </form>
 
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <input ref={el => (this.input = el)}/>
-          <button type="submit">New</button>
-        </form>
+      <ul>
+        {todos.map( todo => (
+          <li key={todo.id}>
+            {todo.complete ? <s>{todo.text}</s> : todo.text}
 
-        <ul>
-          {todos.map( todo => (
-            <li key={todo.id}>
-              {todo.complete ? <s>{todo.text}</s> : todo.text}
-
-              <div>
-                <button onClick={() => toggleTodo(todo.id)}>Toogle</button>
-                <button onClick={() => removeTodo(todo.id)}>Remove</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  todos: state.todos
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(TodoActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList);
+            <div>
+              <button onClick={() => dispatch(toggleTodo(todo.id))}>Toogle</button>
+              <button onClick={() => dispatch(removeTodo(todo.id))}>Remove</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
